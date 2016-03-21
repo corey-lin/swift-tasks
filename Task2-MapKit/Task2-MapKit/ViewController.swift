a008199de717d2fd139e5fcb00dffe95d67f6f32
Corey Lin <lyfone@gmail.com>
2016-03-20 23:08:31 -0700

    Task2-MapKit initial commit

---
+++ Task2-MapKit/Task2-MapKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Task2-MapKit
+//
+//  Created by coreylin on 3/20/16.
+//  Copyright © 2016 coreylin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
