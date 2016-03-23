9f701b256f0e68b5893b1404bb748143ee7101a5
Corey Lin <lyfone@gmail.com>
2016-03-22 23:59:46 -0700

    add UI components for settings

--- Task2-MapKit/Task2-MapKit/ViewController.swift
+++ Task2-MapKit/Task2-MapKit/ViewController.swift
@@ -9,19 +9,32 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegate {
+class ViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegate, UITableViewDataSource, UIPopoverPresentationControllerDelegate {
   @IBOutlet weak var mapView: MKMapView!
+  @IBOutlet weak var showOptionBtn: UIBarButtonItem!
+  var contentController: UITableViewController!
 
+  @IBAction func showMapOptions(sender: AnyObject) {
+    contentController.tableView.dataSource = self
+    contentController.modalPresentationStyle = .Popover
+    let popPC = contentController.popoverPresentationController!
+    popPC.barButtonItem = showOptionBtn
+    popPC.delegate = self
+    self.presentViewController(contentController, animated: true, completion: nil)
+  }
   @IBAction func showSearchBar(sender: AnyObject) {
     let searchController = UISearchController(searchResultsController: nil)
     searchController.searchBar.delegate = self
     searchController.hidesNavigationBarDuringPresentation = false
     presentViewController(searchController, animated: true, completion: nil)
   }
+
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
-    self.mapView.delegate = self
+    mapView.delegate = self
+    contentController = UITableViewController()
+    contentController.tableView.dataSource = self
   }
 
   override func didReceiveMemoryWarning() {
@@ -32,8 +45,8 @@ class ViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegate {
   func searchBarSearchButtonClicked(searchBar: UISearchBar) {
     searchBar.resignFirstResponder()
     self.dismissViewControllerAnimated(true, completion: nil)
-    if self.mapView.annotations.count != 0 {
-      self.mapView.removeAnnotations(self.mapView.annotations)
+    if mapView.annotations.count != 0 {
+      mapView.removeAnnotations(self.mapView.annotations)
     }
 
     let localSearchRequest = MKLocalSearchRequest()
@@ -58,6 +71,7 @@ class ViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegate {
       self.mapView.addAnnotation(myAnnotation)
     })
   }
+
   func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
     if annotation is MyAnnotation {
       var myAnnotationView = mapView.dequeueReusableAnnotationViewWithIdentifier("MyPinView")
@@ -71,6 +85,50 @@ class ViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegate {
     } else {
       return nil
     }
+  }
+
+// MARK: - UITableViewDataSource
+
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 2
+  }
+
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    var cell:UITableViewCell!
+    if cell == nil {
+      cell = UITableViewCell(style: .Default, reuseIdentifier: "cell")
+    }
+    if indexPath.row == 0 {
+      cell = tableView.dequeueReusableCellWithIdentifier("cellMapType")
+      if cell == nil {
+        cell = UITableViewCell(style: .Default, reuseIdentifier: "cellMapType")
+      }
+      let mapType = UISegmentedControl(items:["Standard", "Satellite", "Hybrid"])
+      mapType.center = cell.center
+      cell.addSubview(mapType)
+    } else {
+      let showPOI = UISwitch()
+      cell.textLabel?.text = "Show Point Of Interest"
+      cell.accessoryView = showPOI
+    }
+    return cell;
+  }
+
+// MARK: - UIPopoverPresentationControllerDelegate
+
+  func adaptivePresentationStyleForPresentationController(controller: UIPresentationController) -> UIModalPresentationStyle {
+    return .FullScreen
+  }
+
+  func presentationController(controller: UIPresentationController, viewControllerForAdaptivePresentationStyle style: UIModalPresentationStyle) -> UIViewController? {
+    let navController = UINavigationController(rootViewController: controller.presentedViewController)
+    controller.presentedViewController.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Done, target: self, action: "done")
+    return navController
+  }
+
+// MARK: -
+  func done() {
+
   }
 }
 
