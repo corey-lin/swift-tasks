90b262b6a113bd96a75a74ef268856e03990f9cc
Corey Lin <lyfone@gmail.com>
2016-04-01 23:57:27 -0700

    start developing region map

---
+++ Task2-MapKit/Task2-MapKit/RegionMapViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RegionMapViewController.swift
+//  Task2-MapKit
+//
+//  Created by coreylin on 4/1/16.
+//  Copyright © 2016 coreylin. All rights reserved.
+//
+
+import MapKit
+
+class RegionMapViewController: UIViewController {
+  @IBOutlet weak var mapView: MKMapView!
+  @IBAction func searchRegion(sender: AnyObject) {
+  }
+}
