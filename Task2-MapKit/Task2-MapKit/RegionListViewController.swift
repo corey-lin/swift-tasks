3a4032e4627814c9e0227285d02587a8dcc126fe
Corey Lin <lyfone@gmail.com>
2016-04-06 22:42:56 -0700

    implement RegionListViewController

---
+++ Task2-MapKit/Task2-MapKit/RegionListViewController.swift
@@ -0,0 +1,37 @@
+//
+//  RegionListViewController.swift
+//  Task2-MapKit
+//
+//  Created by coreylin on 4/6/16.
+//  Copyright © 2016 coreylin. All rights reserved.
+//
+
+import UIKit
+
+class RegionListViewController: UIViewController {
+  @IBOutlet weak var regionList: UITableView!
+  var regions = ["Verkhoyansk (Russia)","Fraser, Colo (United States)","Hell (Norway)","Barrow (Alaska)","Oymyakon (Russia)"]
+  var latitudes = [67.550592,39.944987,63.445171,71.290556,63.464138]
+  var longitudes = [133.399340,-105.817232,10.905217,-156.788611,142.773727]
+
+  override func viewDidLoad() {
+    regionList.delegate = self;
+    regionList.dataSource = self;
+  }
+}
+
+extension RegionListViewController: UITableViewDataSource, UITableViewDelegate {
+
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return regions.count
+  }
+
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    var cell = tableView.dequeueReusableCellWithIdentifier("regionCell")
+    if cell == nil {
+      cell = UITableViewCell(style: .Default, reuseIdentifier: "regionCell")
+    }
+    cell?.textLabel?.text = regions[indexPath.row]
+    return cell!
+  }
+}
--- Task2-MapKit/Task2-MapKit/RegionMapViewController.swift
+++ Task2-MapKit/Task2-MapKit/RegionMapViewController.swift
@@ -9,39 +9,11 @@
 import MapKit
 
 class RegionMapViewController: UIViewController {
-  var regionList: UITableViewController!
-  var regions = ["Verkhoyansk (Russia)","Fraser, Colo (United States)","Hell (Norway)","Barrow (Alaska)","Oymyakon (Russia)"]
-  var latitudes = [67.550592,39.944987,63.445171,71.290556,63.464138]
-  var longitudes = [133.399340,-105.817232,10.905217,-156.788611,142.773727]
+
   @IBOutlet weak var mapView: MKMapView!
-  @IBOutlet weak var searchBarItem: UIBarButtonItem!
   
-  @IBAction func searchRegion(sender: AnyObject) {
-    regionList.modalPresentationStyle = .Popover
-    let popoverController = regionList.popoverPresentationController
-    popoverController?.barButtonItem = searchBarItem
-    presentViewController(regionList, animated: true, completion: nil)
-  }
   
   override func viewDidLoad() {
-    regionList = UITableViewController()
-    regionList.tableView.delegate = self
-    regionList.tableView.dataSource = self
-  }
-}
 
-extension RegionMapViewController: UITableViewDataSource, UITableViewDelegate {
-  
-  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return regions.count
-  }
-  
-  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-    var cell = tableView.dequeueReusableCellWithIdentifier("regionCell")
-    if cell == nil {
-      cell = UITableViewCell(style: .Default, reuseIdentifier: "regionCell")
-    }
-    cell?.textLabel?.text = regions[indexPath.row]
-    return cell!
   }
 }
