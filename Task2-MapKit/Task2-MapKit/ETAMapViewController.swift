b0b63318c51a50cb884b5de0f58d81bbf4f22c6e
Corey Lin <lyfone@gmail.com>
2016-04-12 14:14:01 -0700

    prepare to implement ETA map view

---
+++ Task2-MapKit/Task2-MapKit/ETAMapViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ETAMapViewController.swift
+//  Task2-MapKit
+//
+//  Created by coreylin on 4/12/16.
+//  Copyright © 2016 coreylin. All rights reserved.
+//
+
+import UIKit
+
+class ETAMapViewController: UIViewController {
+
+}
