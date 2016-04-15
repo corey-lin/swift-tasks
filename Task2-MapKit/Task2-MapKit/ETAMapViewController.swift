1fa78bc2587b5d0082f36c1af609915c72be7ea5
Corey Lin <lyfone@gmail.com>
2016-04-14 23:49:49 -0700

    add location info

--- Task2-MapKit/Task2-MapKit/ETAMapViewController.swift
+++ Task2-MapKit/Task2-MapKit/ETAMapViewController.swift
@@ -12,14 +12,27 @@ import MapKit
 class ETAMapViewController: UIViewController {
   @IBOutlet weak var mapView: MKMapView!
 
+  let coordinates = [
+    [37.4848474,-122.1505746],
+    [37.4220005,-122.0845547]
+  ]
+  let names = ["Facebook HQ", "Google HQ"]
   var locationManager: CLLocationManager!
 
   override func viewDidLoad() {
     locationManager = CLLocationManager()
     locationManager.requestWhenInUseAuthorization()
     mapView.showsUserLocation = true
+    mapView.delegate = self
   }
 
   @IBAction func nextPlace(sender: AnyObject) {
+  }
+}
+
+extension ETAMapViewController: MKMapViewDelegate {
+  func mapView(mapView: MKMapView, didUpdateUserLocation userLocation: MKUserLocation) {
+    let region = MKCoordinateRegion(center: userLocation.coordinate, span: MKCoordinateSpanMake(2, 2))
+    mapView.setRegion(region, animated: true)
   }
 }
